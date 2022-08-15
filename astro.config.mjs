be2ee71d368c4be816f09af3f062c1f303b40811
alexwhitmore <heyimalexw@gmail.com>
2022-08-14 20:14:17 -0400

    Clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+
 import netlify from '@astrojs/netlify';
 import solid from '@astrojs/solid-js';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
   integrations: [solid()],
