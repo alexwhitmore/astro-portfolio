238575284f6d7e06990455fd271e18d12fb5f16c
alex <57722812+alexwhitmore@users.noreply.github.com>
2024-01-18 14:59:55 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import solid from '@astrojs/solid-js';
 
 export default defineConfig({
-  adapter: netlify(),
   integrations: [solid()],
 });
